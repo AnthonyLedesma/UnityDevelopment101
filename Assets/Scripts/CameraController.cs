ae64dae009b2ad93e3db09a0139da6fb7a960fc3
paranoia1906 <anthonymledesma@gmail.com>
2018-10-10 09:00:36 -0700

    Added Prefab for pickup objects. Added rotator script. New material for pickup object.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
+	
 	public GameObject player; 
 	private Vector3 offset;
-	
+
 	void Start () {
 		offset = transform.position - player.transform.position;
 	}
