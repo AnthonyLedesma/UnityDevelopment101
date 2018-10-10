b586a5a7125328c517b3ba8f03ca454b37116fcb
paranoia1906 <anthonymledesma@gmail.com>
2018-10-10 07:37:48 -0700

    Added player movement script to the sphere object per scripting API.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,14 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+    private Rigidbody bod;
+    void Start()
+    {
+        bod = GetComponent<Rigidbody>();
+    }
 
-	void Update () {
-		float moveHorizontal = Input.GetAxis("Horizontal");
-		float moveVertical = Input.GetAxis("Vertical");
-	}
 
-	void FixedUpdate () {
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+		
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
-	}
+		bod.AddForce (movement * speed);
+    }
 }
