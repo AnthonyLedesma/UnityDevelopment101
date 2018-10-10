ef8c73e57ff63f8ce5023c21f5079f13a2de9b2b
paranoia1906 <anthonymledesma@gmail.com>
2018-10-10 07:09:03 -0700

    Starting the project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	void Update () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+	}
+
+	void FixedUpdate () {
+
+	}
+}
